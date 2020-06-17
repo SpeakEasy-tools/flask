250e3307121f6026e7f8bf5e38539d34e2e52a2a
Matthew Peveler <matt.peveler@gmail.com>
2020-06-17 02:48:31 -0400

    comment out print lines

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ def firebase_auth(f):
     def decorated_function(*args, **kwargs):
         auth_header = request.headers.get('authorization', None)
         if auth_header is None:
-            print('no auth token')
+            # print('no auth token')
             return redirect('/')
         id_token = auth_header.split(' ', 1)[1]
         try:
             auth.verify_id_token(id_token)
         except BaseException as e:
-            print('invalid token')
-            print(e)
+            # print('invalid token')
+            # print(e)
             return redirect('/')
         return f(*args, **kwargs)
     return decorated_function
