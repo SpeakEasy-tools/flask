1fef2b9eac7642e1ec402887fd811c7dba3ac7b4
JustinGOrefice <JustinGOrefice@gmail.com>
2020-06-18 21:09:32 -0400

    added Sudoku

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 from functools import wraps
 import firebase_admin
 from firebase_admin import auth, credentials
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, jsonify, after_this_request
+from static.Sudoku.Generator import Generator
+import queue
 
 app = Flask(__name__)
 firebase_admin.initialize_app(credentials.Certificate('gcloud.json'))
+
+
+def makeBoard(difficulty):
+    with app.app_context():
+        difficulties = {
+            'easy': (35, 0), 
+            'medium': (81, 5), 
+            'hard': (81, 10), 
+            'extreme': (81, 15)
+        }
+        gen = Generator()
+        gen.randomize(100)
+        pair = difficulties[difficulty]
+        gen.reduce_via_logical(pair[0])
+        if pair[1] != 0:
+            gen.reduce_via_random(pair[1])
+        final = gen.board.copy()
+        sudoku_boards[difficulty].put(jsonify(final.stringify()))
+
+#code for generating sudoku boards, very slow so comment out if working on something else
+sudoku_boards = {}
+for difficulty in ('easy', 'medium', 'hard', 'extreme'):
+    sudoku_boards[difficulty] = queue.LifoQueue()
+for key in sudoku_boards:
+    for i in range(2): #change number to change number of boards to store
+        makeBoard(key)
+        print("Generated board " + str(i) + " for " + key)
 
 
 def firebase_auth(f):
@@ -41,12 +70,18 @@ def uno():
     return 'uno'
 
 
-@app.route('/sudoku/<int:difficulty>')
-@firebase_auth
-def sudoku(difficulty):
-    return "{board:[[9, , 8, 2, 1, , 6, , ],[1, 3, 4, , 9, 6, , , 2],[2, , 7, , 5, , 1, , ],[ , 1, 3, 7, , , 9, 2, " \
-           "5],[ , , 9, , 2, , 7, , ],[ , , , 9, 3, 5, 8, , 6],[ , 2, , , , 9, 3, , 7],[ , , 1, , , , 4, 8, ],[4, , " \
-           "6, , , , , , 1]]} "
+@app.route('/sudoku', methods=['POST'])
+# @firebase_auth
+def sudoku():
+    @after_this_request
+    def after_request(response):
+        print(arg)
+        makeBoard(arg)
+        response.headers.add('Access-Control-Allow-Origin', '*') #change * to SpeakEasy server 
+        return response
+    arg = request.args.get('difficulty')
+    board = sudoku_boards[arg].get()
+    return board
 
 
 @app.route('/test')
